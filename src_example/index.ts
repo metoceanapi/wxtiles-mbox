508591f2321d3cbcca0b8f1c052a2acb759d89e4
SergeiMelman <s.melman@metocean.co.nz>
2022-11-03 15:49:56 +1300

    bug2

--- src_example/index.ts
+++ src_example/index.ts
@@ -14,7 +14,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibW91cm5lciIsImEiOiJWWnRiWG1VIn0.j6eccFHpE3Q04
 
 	map.showTileBoundaries = true;
 
-	const customSource = {
+	const customSource2 = {
 		type: 'custom' as 'custom',
 		dataType: 'raster' as 'raster',
 		id: 'customSource',
@@ -23,6 +23,36 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibW91cm5lciIsImEiOiJWWnRiWG1VIn0.j6eccFHpE3Q04
 			const im = createImageBitmap(await (await fetch(`http://tile.openstreetmap.org/${tile.z}/${tile.x}/${tile.y}.png`, init)).blob());
 			await Promise.all([im, new Promise((resolve) => setTimeout(resolve, 2500))]);
 			return im;
+		},
+	};
+
+	const customSource = {
+		type: 'custom' as 'custom',
+		dataType: 'raster' as 'raster',
+		id: 'customSource',
+
+		async loadTile(tile: { z: any; x: any; y: any }, init: RequestInit | undefined): Promise<any> {
+			const canvas = document.createElement('canvas');
+			canvas.width = 256;
+			canvas.height = 256;
+			const ctx = canvas.getContext('2d')!;
+			ctx.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+			ctx.fillRect(0, 0, 256, 256);
+			return canvas; //new ImageData(256, 256);
+		},
+
+		startAnimating() {
+			const nextFrame = () => {
+				requestAnimationFrame(nextFrame);
+
+				this.update();
+			};
+			nextFrame();
+		},
+		update() {}, // reassigned by mapbox
+
+		onAdd() {
+			this.startAnimating();
 		},
 	};
 
