76351b8596a2bbf2d83ad4b5b13458e60691072c
SergeiMelman <s.melman@metocean.co.nz>
2022-11-14 11:35:24 +1300

    new demo

---
+++ build-for-docs.js
@@ -0,0 +1,23 @@
+const esbuild = require('esbuild');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+esbuild
+	.build({
+		entryPoints: ['src_example/index.ts'],
+		bundle: true,
+		plugins: [],
+		loader: {
+			'.png': 'base64',
+			'.woff': 'base64',
+			'.fs': 'text',
+			'.vs': 'text',
+		},
+		target: 'es2020',
+		format: 'iife',
+		outfile: 'docs/wxtilembox/wxtilembox.js',
+		sourcemap: false,
+		minify: true,
+	})
+	.catch((e) => console.error(e.message));
