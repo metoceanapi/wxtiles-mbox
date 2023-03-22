5a45e7dfa0335f70339f92801c8f03c272b00a22
SergeiMelman <s.melman@metocean.co.nz>
2023-03-22 16:47:29 +1300

    fix: shaders - release (I believe)

--- src/customlayer/customsahders/custom.fs
+++ src/customlayer/customsahders/custom.fs
@@ -1,27 +1,34 @@
+// 2D vector field animation
+// Author: Sergei Melman (sergeimelman0@gmail.com)
+// Year: 2023
+// Distributed under the MIT License.
+
 #version 100
 precision highp float;
 
-uniform sampler2D u_noiseTexture;
-uniform sampler2D u_tileTexture;
-uniform float u_opacity;
-
-uniform float u_Lmax;
-
-uniform sampler2D u_V;
-uniform float u_Vmul;
-uniform float u_Vmin;
-
-uniform sampler2D u_U;
-uniform float u_Umul;
-uniform float u_Umin;
-
-uniform float u_animationSpeed;
-uniform float u_animationTime;
-
 #define PI 3.1415926538
 
-varying vec2 v_pos0;
-varying vec2 v_posV;
+uniform sampler2D u_noiseTexture; // noise texture
+uniform sampler2D u_tileTexture; // main tiles texture (background)
+uniform float u_opacity; // general opacity of the layer
+
+uniform float u_Lmax; // maximum possible length of the vector field
+
+uniform sampler2D u_V; // V component of the vector field
+uniform float u_Vmul; // V component multiplier to convert [0, 65535] to proper value
+uniform float u_Vmin; // V component minimum value to convert [0, 65535] to proper value
+
+uniform sampler2D u_U; // U component of the vector field
+uniform float u_Umul; // U component multiplier to convert [0, 65535] to proper value
+uniform float u_Umin; // U component minimum value to convert [0, 65535] to proper value
+
+uniform float u_vectorFieldFactor; // multiply vector field by this value
+uniform float u_animationTimePosition; // animation time - phase of animation
+uniform float u_animationIntensity; // animation intensity
+uniform float u_wavesCount; // number of waves, beter more than one
+
+varying vec2 v_pos0; // main tiles texture coordinates
+varying vec2 v_posV; // vector field texture coordinates. Shifted as vector field is 258x258, but main tiles are 256x256
 
 // The Noise texture
 vec3 noise(vec2 uv) {
@@ -38,148 +45,43 @@ vec3 noise(vec2 uv) {
 }
 
 void main() {
-    // if(u_animationSpeed > 0.2) {
-    //     gl_FragColor = vec4(noise(v_pos0), 1.0);
-    //     return;
-    // }
-
-	// read color from the main tiles
+	// read color from the main tiles texture
     vec4 color = texture2D(u_tileTexture, v_pos0) * u_opacity;
 	// simple render for non animated layers
-    if(u_animationSpeed < 0.2) {
+    if(u_vectorFieldFactor < 0.2) {
         gl_FragColor = color;
         return;
     }
 
-	// read vector U an V component, use only HIGH byte from two 8bit channels - enough for visualization
+	// read vector U an V component, use only ALPHA chennel as a HIGH byte from two 8bit channels - enough for visualization
     float U = texture2D(u_U, v_posV).a;
     float V = texture2D(u_V, v_posV).a;
 
 	// Fector field value at pixel (convert [0,65535] to proper U/V values)
-    vec2 vec = vec2(-(U * u_Umul + u_Umin), V * u_Vmul + u_Vmin) / u_Lmax; // normalize by maximum possible length of the vector field
-    float lvec = length(vec);
-    if(lvec < 0.001) {
+    vec2 vectorValueAtPixel = vec2(-(U * u_Umul + u_Umin), V * u_Vmul + u_Vmin) / u_Lmax; // normalize by maximum possible length of the vector field
+    float vectorValueAtPixelLength = length(vectorValueAtPixel);
+    if(vectorValueAtPixelLength < 0.001) {
         gl_FragColor = color;
         return;
     }
 
-    float t = fract(u_animationTime); // -> [0, 1]
-
-    vec3 col = vec3(0.0);
-    const float d = 0.3;
-    for(float i = 0.0; i < 1.0; i += d) {
-        float t1 = fract(i + t); // -> [0, 1]
-        float t2 = fract(t1 + 0.5);
-        col += noise(v_pos0 + vec * u_animationSpeed * t1) * (1. - 2. * abs(t1 - 0.5)) * d;
-        col += noise(v_pos0 + vec * u_animationSpeed * t2) * (1. - 2. * abs(t2 - 0.5)) * d;
+    vec3 wavesCumulativeNoise = vec3(0.0);
+    float d = 1.0 / u_wavesCount;
+    for(float i = 0.0; i < 10.0; i++) { // the limit of u_wavesCount is 10
+        if(i >= u_wavesCount) // workaround for WebGL 1.0, GLSL 1.0 shader does not support non const variables in for loop
+            break;
+        // waves along the vector field
+        float phase = fract(i * d + u_animationTimePosition); // -> [0, 1]
+        float intensity = 1. - 2. * abs(phase - 0.5); // triangle function [0, 0.5, 1]-> [0, 1, 0]; The maximum intensity is at the middle of the time interval (t = 0.5)
+        vec3 noiseValue = noise(v_pos0 + vectorValueAtPixel * phase * u_vectorFieldFactor);
+        wavesCumulativeNoise += (noiseValue - 0.5) * intensity; // add noise to the color
     }
 
-    col -= (0.5);
-    color.xyz += color.xyz * col * lvec * 4.; // use pixel's color if pixel is not transparent
+    // Now we adjust the noise's intensity
+    wavesCumulativeNoise *= vectorValueAtPixelLength *  // intensity depends on the vector field length
+        u_animationIntensity / // animation intensity
+        u_wavesCount; // avarageing by number of waves
 
+    color.xyz *= 1.0 + wavesCumulativeNoise; // distort the color by noise 
     gl_FragColor = color;
 }
-
-/*
-// 2D vector field: The Vortex
-vec2 vField( vec2 uv )
-{
-    // uv - Normalized pixel coordinates (from 0 to 1)
-    
-    uv*= 2.;
-    uv -= vec2(1.); // fragCoord -> [-1, 1]
-    
-    uv = normalize(vec2(uv.y, -uv.x)) * sin(3.145*length(uv)*2.);
-    
-    return uv;
-}
-
-// The Noise texture
-vec4 noise( vec2 uv )
-{
-    // uv - Normalized pixel coordinates (from 0 to 1)
-
-    float v = length(sin(uv*200.))/sqrt(2.0);
-    return vec4(v, abs(0.5-v), 1. - v, 1.);
-}
-
-
-// image
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
-    
-    
-    vec2 vec = vField(uv).xy / 09.;
-    
-    float t1 = fract(iTime);
-    float t2 = fract(t1 +0.5);
-    vec3 col1 = noise(uv + vec * t1).rgb * (1. - 2.*abs(t1-0.5));
-    vec3 col2 = noise(uv + vec * t2).rgb * (1. - 2.*abs(t2-0.5));
-
-    vec3 col = (col1 + col2) ;
-    // Output to screen
-    fragColor = vec4(col* length(vec*9.),1.0);
-}
-//*/
-
-/*
-// 2D vector field: The Vortex
-vec2 vField( vec2 uv )
-{
-    // uv - Normalized pixel coordinates (from 0 to 1)
-    
-    uv*= 2.;
-    uv -= vec2(1.); // fragCoord -> [-1, 1]
-    
-    uv = normalize(vec2(uv.y, -uv.x)) * sin(3.145*length(uv));
-    
-    return uv;
-}
-
-float inoise(float t) {
-    return fract(sin(t * 7.1)*8.5453123);
-}
-
-#define PI 3.14156
-// The Noise texture
-vec3 noise( vec2 uv )
-{
-
-     //return vec3(inoise(uv.x*uv.y));
-
-     return texture(iChannel0, uv).rgb;
-    // uv - Normalized pixel coordinates (from 0 to 1)
-    uv.x /= 1.;
-
-    float v = (length(sin(PI*uv*30.)*sin(PI*uv*31.)*sin(PI*uv*32.5)));
-    return vec3(v);
-}
-
-
-// image
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.y;
-
-    vec2 vec = vField(uv) / 5. ;
-   
-    float t = fract(iTime/2.); // -> [0, 1]
-    
-    vec3 col;
-    
-    float d = 0.3;
-    
-    for(float i = 0.0; i < 1.0; i += d){
-        float t1 = fract(i + t); // -> [0, 1]
-        float t2 = fract(t1 + 0.5);
-        col += noise(uv + vec*t1) * (1. - 2.*abs(t1-0.5))*d;
-        col += noise(uv + vec*t2) * (1. - 2.*abs(t2-0.5))*d;
-    }
-
-    // Output to screen
-    fragColor = vec4(col,1.0)*length(vec)*5.;
-}
-*/
